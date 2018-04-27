345b2fb211c06cd82f3ebf557c8fee2a28073445
estebanmagallonperez <esteban1995perez@gmail.com>
2018-04-27 00:19:10 -0700

    update so that only html files can get the header and footer  wrapped around the file

--- webModules/shittyApache.js
+++ webModules/shittyApache.js
@@ -1,21 +1,13 @@
 const fs = require('fs');
-var ss = require('stream-stream');
+const ss = require('stream-stream');
 const mime = require('mime-types');
+const websiteBase = "website";
 
-
-
-var websiteBase = "website";
 exports.fetchFile = function (request, response) {
+	//prep the url with the directory on the server
 	request.url = websiteBase+request.url;
-	console.log(request.url);
-	if(request.headers["referer"] == undefined)
-	{
-		console.log("prepend header, and append footer");
-	}
-	//console.log(request.headers);
 	fs.stat(request.url, function(err, stat) {
-		//console.log("known mimetype",mime.lookup(request.url) == false);
-		//console.log(err);
+		//check for errors and return the error page if any errors exist. This is you custom 404 page
 		if(mime.lookup(request.url) == false ||err != null && err.code == 'ENOENT')
 		{
 			var stat = fs.statSync(websiteBase+"/error.html");
@@ -26,10 +18,12 @@ exports.fetchFile = function (request, response) {
 			var readStream = fs.createReadStream(websiteBase+"/error.html");
 			readStream.pipe(response);
 		}else if(err == null) {
+			//This is where the magic happens. We are loading the fill template if it a first time visit to the page
+			//This should allow a crawler to do its thing
 			var files = [];
 			var type = mime.lookup(request.url);
-			console.log
-			if(request.headers["referer"] == undefined)
+			console.log("type is: ",type);
+			if(request.headers["referer"] == undefined && type == 'text/html')
 			{
 				var files = ['header.html', request.url,'footer.html'];
 				var stream = ss();
@@ -38,13 +32,11 @@ exports.fetchFile = function (request, response) {
 					stream.write(fs.createReadStream(f));
 				});
 				stream.end();
-
 				response.writeHead(200, {'Content-Type' : 'text/html'});
 				stream.pipe(response);
 				return;
 			}else
 			{
-				console.log("ajax load");
 				response.writeHead(200, {
 					'Content-Type': mime.lookup(request.url),
 					'Content-Length': stat.size
@@ -53,19 +45,6 @@ exports.fetchFile = function (request, response) {
 				var readStream = fs.createReadStream(request.url);
 				readStream.pipe(response);
 			}
-
-
-
-			/*var stream = ss();
-
-			files.forEach(function(f) {
-				stream.write(fs.createReadStream(f));
-			});
-			stream.end();
-
-			console.log(request.url);
-			var readStream = fs.createReadStream(request.url);
-			readStream.pipe(response);*/
 		} else {
 			console.log('Some other error: ', err.code);
 		}
